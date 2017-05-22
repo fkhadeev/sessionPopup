013b2ccbc908712887f7f13dbf0241897ed6d1b4
HannibalLector <fans7288@gmail.com>
2017-05-22 15:16:07 +0500

    for all pages

--- ajax/session.php
+++ ajax/session.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
 
-if (isset($_POST['session_param_name']) && !isset($_SESSION[$_POST['session_param_name']])) {
-    $_SESSION[$_POST['session_param_name']] = 1;
-    echo 'yes';
-} else {
-    echo 'no';
+if (isset($_POST['session_param_name']) && isset($_POST['popup_time']) && isset($_POST['currentTime'])) {
+    if (!isset($_SESSION[$_POST['session_param_name']])) {
+        $_SESSION[$_POST['session_param_name']] = $_POST['popup_time'];
+        echo 'no';
+    } elseif ($_SESSION[$_POST['session_param_name']] == 'none')
+        echo 'clear';
+    elseif ($_SESSION[$_POST['session_param_name']] <= $_POST['currentTime']) {
+        $_SESSION[$_POST['session_param_name']] = 'none';
+        echo 'yes';
+    }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 var settings = {
-    'timeout': 3000,
+    'timeout': 10000, // millisecond
     'url': '/ajax/session.php',
     'sessionParam': 'js_popup'
 };
@@ -8,15 +8,21 @@ function showPopup() {
     alert('i\'am popup)');
 }
 
-function checkSession() {
-    $.post(settings.url, {session_param_name: settings.sessionParam}).done(
+function checkSession(t) {
+    var dateTime = Date.now() + settings.timeout;
+    $.post(settings.url, {session_param_name: settings.sessionParam, popup_time: dateTime, currentTime: Date.now()}).done(
         function (data) {
-            if (data && data == 'yes')
-                showPopup();
+            if (data) {
+                if (data == 'yes') {
+                    showPopup();
+                    clearInterval(t);
+                } else if (data == 'clear')
+                    clearInterval(t);
+            }
         }
     );
 }
 
 $(function () {
-    setTimeout(function() {checkSession()}, settings.timeout);
+    var t = setInterval(function() {checkSession(t)}, 1000);
 });
